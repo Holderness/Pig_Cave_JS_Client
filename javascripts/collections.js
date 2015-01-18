0e6eb0d91d7056fa26f831702f52e20cf21ddfe1
Holderness <andersonholderness@gmail.com>
2015-01-18 13:07:18 -0500

    changes ip address in collections

--- javascripts/collections.js
+++ javascripts/collections.js
@@ -1,14 +1,14 @@
 var DreamList = Backbone.Collection.extend({
 	model: Dream,
-	url: "http://192.241.243.100:3001/dreams"
+	url: "http://104.131.205.122/dreams"
 });
 
 var StoryList = Backbone.Collection.extend({
 	model: Story,
-	url: "http://192.241.243.100:3001/stories"
+	url: "http://104.131.205.122/stories"
 });
 
 var ThoughtList = Backbone.Collection.extend({
 	model: Thought,
-	url: "http://192.241.243.100:3001/thoughts"
+	url: "http://104.131.205.122/thoughts"
 });
